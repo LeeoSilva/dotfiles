7c83416beff84d7dc850bba48ed81ac2e22213fd
LeeoSilva <leonardo.silva1199@etec.sp.gov.br>
2018-04-21 21:48:36 -0300

    21/04/18

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ CONKY="/home/$USER/.conkyrc";
 DUNST="/home/leonardo/.config/dunst/dunstrc";
 FONTS="/home/$USER/.fonts/*";
 HTOP="/home/$USER/.config/htop/htoprc";
-I3="/home/$USER/.config/i3/config";
+I3="/home/$USER/.config/i3/*";
 POLYBAR="/home/$USER/.config/polybar/config";
 RANGER="/home/$USER/.config/ranger/*";
 TERMITE="/home/$USER/.config/termite/config";
@@ -17,20 +17,21 @@ VIM="/home/$USER/.vimrc";
 ZSH="/home/$USER/.zshrc";
 NCMPCPP="/home/$USER/.ncmpcpp/config";
 MPD="/home/$USER/.config/mpd/mpd.conf";
+$PATH=$(pwd)_
 
 echo "Updating...";
-cp -uvf $COMPTON /MeuBolso/Codes/dotfiles/compton/compton.conf;
-cp -uvf $HTOP /MeuBolso/Codes/dotfiles/htop/htoprc;
-cp -uvf $TERMITE /MeuBolso/Codes/dotfiles/termite/config;
-cp -uvf $I3 /MeuBolso/Codes/dotfiles/i3/config;
-cp -uvf $ZSH /MeuBolso/Codes/dotfiles/zsh/zshrc;
-cp -uvf $CONKY /MeuBolso/Codes/dotfiles/conky/conkyrc;
-cp -uvf $VIM /MeuBolso/Codes/dotfiles/vim/vimrc;
-cp -uvf $TMUX /MeuBolso/Codes/dotfiles/tmux/tmux.conf;
-cp -uvf $RANGER /MeuBolso/Codes/dotfiles/ranger/;
-cp -uvf $POLYBAR /MeuBolso/Codes/dotfiles/polybar/;
-cp -uvf $FONTS /MeuBolso/Codes/dotfiles/fonts/;
-cp -uvf $DUNST /MeuBolso/Codes/dotfiles/dunst/;
-cp -uvf $NCMPCPP /MeuBolso/Codes/dotfiles/ncmpcpp/;
-cp -uvf $MPD /MeuBolso/Codes/dotfiles/mpd/;
+cp -uvf $COMPTON $PATH/compton/compton.conf
+cp -uvf $HTOP $PATH/htop/htoprc
+cp -uvf $TERMITE $PATH/termite/config
+cp -uvf $I3 $PATH/i3/
+cp -uvf $ZSH $PATH/zsh/zshrc
+cp -uvf $CONKY $PATH/conky/conkyrc;
+cp -uvf $VIM $PATH/nvim/init.vim;
+cp -uvf $TMUX $PATH/tmux/tmux.conf;
+cp -uvf $RANGER $PATH/ranger/;
+cp -uvf $POLYBAR $PATH/polybar/;
+cp -uvf $FONTS $PATH/fonts/;
+cp -uvf $DUNST $PATH/dunst/;
+cp -uvf $NCMPCPP $PATH/ncmpcpp/;
+cp -uvf $MPD $PATH/mpd/;
 echo "Updated!";
