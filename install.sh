edab6cba76fbd423b8c571ac3ab57b38cf8375a4
LeeoSilva <LeeoSilva0909@gmail.com>
2018-09-14 19:59:46 -0300

    ZSH code cleaned

--- install.sh
+++ install.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
 
-# Self-Explanatory name '-'
-COMPTON="/home/$USER/.config/compton.conf";
-CONKY="/home/$USER/.conkyrc";
+# If the script is running without root permissions
+# Enter root.
+checkRoot = id -u $USER
+if [ checkRoot == "1000" ]
+	then # Running with sudo
+		echo "You are running this with root permissions and the files will be installed in /root";
+		echo "Installing in 3...";
+		sleep 1;
+		echo "Installing in 2...";
+		sleep 1;
+		echo "Installing in 1...";
+		sleep 1;
+fi
+
+# Variables for the PATH of all config files
+COMPTON="~/.config/compton.conf";
+CONKY="~/.conkyrc";
 DUNST="/home/leonardo/.config/dunst/dunstrc";
-FONTS="/home/$USER/.fonts/";
-HTOP="/home/$USER/.config/htop/htoprc";
-I3="/home/$USER/.config/i3/config";
-POLYBAR="/home/$USER/.config/polybar/config";
-RANGER="/home/$USER/.config/ranger/";
-TERMITE="/home/$USER/.config/termite/config";
-TMUX="/home/$USER/.tmux.conf";
-VIM="/home/$USER/.config/nvim/init.vim";
-ZSH="/home/$USER/.zshrc";
-ZSHDIR="/home/$USER/.zsh/";
-NCMPCPP="/home/$USER/.ncmpcpp/config";
-MPD="/home/$USER/.config/mpd/mpd.conf";
+FONTS="~/.fonts/";
+HTOP="~/.config/htop/htoprc";
+I3="~/.config/i3/config";
+POLYBAR="~/.config/polybar/config";
+RANGER="~/.config/ranger/";
+TERMITE="~/.config/termite/config";
+TMUX="~/.tmux.conf";
+VIM="~/.config/nvim/init.vim";
+ZSH="~/.zshrc";
+ZSHDIR="~/.zsh/";
+NCMPCPP="~/.ncmpcpp/config";
+MPD="~/.config/mpd/mpd.conf";
+
+if [ ! -d "~/.fonts" ] then mkdir -p ~/.fonts fi;
 
 echo "Installing...";
+
 cp -uvf fonts/* $FONTS;
 cp -uvf ncmpcpp/config $NCMPCPP;
 cp -uvf mpd/mpd.conf $MPD;
 cp -uvf i3/config $I3;
 cp -uvf dunst/dunstrc $DUNST
-cp -uvf compton/compton.conf $COMPTON;
 cp -uvf conky/conkyrc $CONKY;
 cp -uvf htop/htoprc $HTOP;
 cp -uvf termite/config $TERMITE;
