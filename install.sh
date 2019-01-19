105778eb40ed8dafc8e0b04cd0bcfe5026e479a7
LeeoSilva <leeosilva0909@gmail.com>
2019-01-19 19:36:13 -0200

    ZSH and TMUX scripts - Install bugs fixed

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ polybar="$HOME/.config/polybar/config";
 ranger="$HOME/.config/ranger/";
 termite="$HOME/.config/termite/config";
 tmux="$HOME/.tmux.conf";
-tmuxsession="$HOME/.tmux/sessions/session1"
+tmuxsession="$HOME/.tmux/sessions/startup"
 vim="$HOME/.config/nvim/init.vim";
 zsh="$HOME/.zshrc";
 zshdir="$HOME/.zsh/";
@@ -36,6 +36,8 @@ color_light_green='\033[1;32m';
 
 if [ ! -d "$HOME/.fonts" ]; then mkdir -p "$fonts"; fi
 if [ ! -d "$HOME/.config" ]; then mkdir -p "$HOME/.config"; fi
+if [ ! -d "$HOME/.zsh" ]; then mkdir -p "$HOME/.zsh"; fi
+if [ ! -d "$HOME/.tmux/sessions/" ]; then mkdir -p "$HOME/.tmux/sessions/"; fi
 
 cp -uf fonts/* "$fonts" 2> /dev/null && echo -e "Fonts [${color_light_green}Installed${color_nc}]";
 cp -uf ncmpcpp/config "$ncmpcpp" 2> /dev/null && echo -e "NCMPCPP config [${color_light_green}Installed${color_nc}]";
@@ -46,9 +48,23 @@ cp -uf conky/conkyrc "$conky" 2> /dev/null && echo -e "CONKY config [${color_lig
 cp -uf htop/htoprc "$htop" 2> /dev/null && echo -e "HTOP config [${color_light_green}Installed${color_nc}]";
 cp -uf termite/config "$termite" 2> /dev/null && echo -e "TERMITE config [${color_light_green}Installed${color_nc}]";
 cp -uf tmux/tmux.conf "$tmux" 2> /dev/null && echo -e "TMUX config [${color_light_green}Installed${color_nc}]";
-cp -urf tmux/sessions/ "$tmuxsession" 2> /dev/null && echo -e "TMUX sessions [${color_light_green}Installed${color_nc}]";
 cp -uf nvim/init.vim "$vim" 2> /dev/null && echo -e "NVIM config [${color_light_green}Installed${color_nc}]";
 cp -uf zsh/zshrc "$zsh" 2> /dev/null && echo -e "ZSH config [${color_light_green}Installed${color_nc}]";
-cp -uf zsh/prompt.sh "$zshdir" 2> /dev/null && echo -e "ZSH prompt script [${color_light_green}Installed${color_nc}]";
 cp -uf ranger/* "$ranger" 2> /dev/null && echo -e "RANGER config [${color_light_green}Installed${color_nc}]";
 cp -uf polybar/config "$polybar" 2> /dev/null && echo -e "POLYBAR config [${color_light_green}Installed${color_nc}]";
+
+# ZSH plugin
+echo -e "Installing [${color_light_green}ZSH Plugins${color_nc}]";
+
+cp -uf zsh/prompt.sh "$zshdir" 2> /dev/null && echo -e "ZSH prompt script [${color_light_green}Installed${color_nc}]";
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions 2> /dev/null # autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting 2> /dev/null # syntax-highlighting
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" 2> /dev/null
+
+# Installing TMUX Sessions
+echo -e "Installing [${color_light_green}TMUX Sessions${color_nc}]";
+cp -urf tmux/sessions/* "$tmuxsession" 2> /dev/null && echo -e "TMUX sessions [${color_light_green}Installed${color_nc}]";
+
+# Finishing up
+echo -e "${color_light_green}All Set-up!${color_nc}";
