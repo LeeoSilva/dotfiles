b2935fa065824ec8222b9a2c42d1eb3e02bd4d7d
LeeoSilva <leeosilva0909@gmail.com>
2020-10-10 18:11:58 -0300

    script for full unicode coverage for arch linux

---
+++ fonts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yay -S ttf-roboto ttf-fira-code noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-font-awesome ttf-dejavu adobe-source-code-pro-fonts
--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
   
 if [ $EUID == "0" ]
 	then
-		echo -e "You are running this with root permissions and the files will be installed in /root";
+		echo -e "You are running this with root permissions and the files will be Installed in /root";
 		echo -e "Installing in 3...";
 		sleep 1;
 		echo -e "Installing in 2...";
@@ -31,6 +31,8 @@ zsh="$HOME/.zshrc";
 zshdir="$HOME/.zsh/";
 ncmpcpp="$HOME/.ncmpcpp/config";
 mpd="$HOME/.config/mpd/mpd.conf";
+fontconfig="$HOME/.config/fontconfig/";
+path="$(pwd)"; # One day i'll fix this '-'
 
 color_nc='\033[0m'; # No Color
 color_light_green='\033[1;32m';
@@ -39,31 +41,39 @@ if [ ! -d "$HOME/.fonts" ]; then mkdir -p "$fonts"; fi
 if [ ! -d "$HOME/.config" ]; then mkdir -p "$HOME/.config"; fi
 if [ ! -d "$HOME/.zsh" ]; then mkdir -p "$HOME/.zsh"; fi
 if [ ! -d "$HOME/.tmux/sessions/" ]; then mkdir -p "$HOME/.tmux/sessions/	"; fi
-if [ ! -d "$HOME/.config/nvim"]; then mkdir -p "$HOME/.config/nvim/	"; fi
+if [ ! -d "$HOME/.config/nvim/" ]; then mkdir -p "$HOME/.config/nvim/	"; fi
+if [ ! -d "$HOME/.config/htop/" ]; then mkdir -p "$HOME/.config/htop/	"; fi
+if [ ! -d "$HOME/.config/htop/" ]; then mkdir -p "$HOME/.config/htop/	"; fi
+if [ ! -d "$HOME/.config/fontconfig/" ]; then mkdir -p "$HOME/.config/fontconfig/"; fi
+
+(echo -e "source $path/zsh/zshrc" > $zsh) 2> /dev/null && echo -e "ZSH config[${color_light_green}Installed${color_nc}];"       # ZSH
+(echo -e "source $path/nvim/init.vim" > $vim) 2> /dev/null && echo -e "NEOVIM config [${color_light_green}Installed${color_nc};"		   # NVIM
+(echo -e "source $path/tmux/tmux.conf" > $tmux) 2> /dev/null && echo -e "TMUX config [${color_light_green}Installed${color_nc}];"	   # TMUX 
+(echo -e "source $path/htop/htoprc" > $htop) 2> /dev/null && echo -e "HTOP config [${color_light_green}Installed${color_nc}]";  # HTOP 
+(echo -e "source $path/termite/config" > $termite) 2> /dev/null && echo -e "TERMITE config [${color_light_green}Installed${color_nc}]"; # TERMITE 
+(echo -e "source $path/polybar/config" > $polybar) 2> /dev/null && echo -e "POLYBAR config [${color_light_green}Installed${color_nc}]"; # POLYBAR 
+(echo -e "source $path/i3/config" > $i3) 2> /dev/null && echo -e "I3 config [$(color_light_green)Installed${color_nc}]"; # I3
+(echo -e "source $path/ncmpcpp/config" > $ncmpcpp) 2> /dev/null && echo -e "NCMPCPP config [${color_light_green}Installed${color_nc}]";
+(echo -e "source $path/mpd/mpd.conf" > $mpd) 2> /dev/null && echo -e "MPD config [${color_light_green}Installed${color_nc}]"; 
+(echo -e "source $path/dunst/dunstrc" > $dunst) 2> /dev/null && echo -e "DUNST config [${color_light_green}Installed${color_nc}]";
 
 cp -uf fonts/* "$fonts" 2> /dev/null && echo -e "Fonts [${color_light_green}Installed${color_nc}]";
-cp -uf ncmpcpp/config "$ncmpcpp" 2> /dev/null && echo -e "NCMPCPP config [${color_light_green}Installed${color_nc}]";
-cp -uf mpd/mpd.conf "$mpd" 2> /dev/null && echo -e "MPD config [${color_light_green}Installed${color_nc}]";
-cp -uf i3/config "$i3" 2> /dev/null && echo -e "I3 config [${color_light_green}Installed${color_nc}]";
-cp -uf dunst/dunstrc "$dunst" 2> /dev/null && echo -e "DUNST config [${color_light_green}Installed${color_nc}]";
-cp -uf conky/conkyrc "$conky" 2> /dev/null && echo -e "CONKY config [${color_light_green}Installed${color_nc}]";
-cp -uf htop/htoprc "$htop" 2> /dev/null && echo -e "HTOP config [${color_light_green}Installed${color_nc}]";
-cp -uf termite/config "$termite" 2> /dev/null && echo -e "TERMITE config [${color_light_green}Installed${color_nc}]";
-cp -uf tmux/tmux.conf "$tmux" 2> /dev/null && echo -e "TMUX config [${color_light_green}Installed${color_nc}]";
-cp -uf nvim/init.vim "$vim" 2> /dev/null && echo -e "NVIM config [${color_light_green}Installed${color_nc}]";
-cp -uf zsh/zshrc "$zsh" 2> /dev/null && echo -e "ZSH config [${color_light_green}Installed${color_nc}]";
-cp -uf ranger/* "$ranger" 2> /dev/null && echo -e "RANGER config [${color_light_green}Installed${color_nc}]";
-cp -uf polybar/config "$polybar" 2> /dev/null && echo -e "POLYBAR config [${color_light_green}Installed${color_nc}]";
+cp -uf fontconfig/* "$fontconfig" 2> /dev/null && echo -e "Emoji support [${color_light_green}Installed${color_nc}]";
+cp -uf $path/ranger/* "$ranger" 2> /dev/null && echo -e "RANGER config [${color_light_green}Installed${color_nc}]";
+cp -urf $path/tmux/sessions/* "$tmuxsession" 2> /dev/null && echo -e "TMUX sessions [${color_light_green}Installed${color_nc}]";
+
+sudo pacman -S --noconfirm noto-fonts-emoji ttf-joypixels 
+
 
 # ZSH plugins 
 echo -e "Installing [${color_light_green}ZSH Plugins${color_nc}]";
 cp -uf zsh/prompt.sh "$zshdir" 2> /dev/null && echo -e "ZSH prompt script [${color_light_green}Installed${color_nc}]";
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" # Oh my zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions  # autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting # syntax-highlighting
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" # ZPrezto 
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k # PowerLevel10K
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" # ZPrezto
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm # Tmux Plugin Manager 
+git clone https://github.com/romkatv/powerlevel10k.git ${ZDOTDIR:-$HOME}/.oh-my-zsh/themes/powerlevel10k
 
 # Installing TMUX Sessions
 echo -e "Installing [${color_light_green}TMUX Sessions${color_nc}]";
