21fcb304f3ab8dfce4f928a195c2605269686eb4
LeeoSilva <leeosilva0909@gmail.com>
2021-10-27 01:26:27 -0300

    added fonts.sh apt package manager version

---
+++ install-fonts.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## Yay package manager - Arch or Arch-based distros 
+#yay -S ttf-roboto ttf-fira-code noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-font-awesome ttf-dejavu adobe-source-code-pro-fonts ttf-joypixels
+
+
+## Apt package manager - Debian or Debian-based distros
+sudo apt install fonts-roboto fonts-firacode fonts-noto fonts-noto-cjk fonts-noto-color-emoji fonts-noto-cjk-extra fonts-font-awesome ttf-dejavu
