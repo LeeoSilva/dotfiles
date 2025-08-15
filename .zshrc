353a0f8cdc1a5cfee529558715f655f859fdfcca
LeeoSilva <leeosilva0909@gmail.com>
2025-08-15 15:51:25 -0300

    Adding Ghostty config, fixed install script and also fixed folder structure.

--- .zshrc
+++ .zshrc
@@ -138,4 +138,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh && source <(fzf --zsh)
+source <(fzf --zsh)
 
--- install.sh
+++ install.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-LOCALBIN="($HOME/.local/bin)";
 TMUX="$HOME/.tmux.conf";
 NVIM="$HOME/.config/nvim/init.vim";
+GHOSTTY="$HOME/.config/ghostty/config";
 ZSH="$HOME/.zshrc";
 CWD="$(pwd)"; 
 
 mkdir -p "$HOME/.config/"
 mkdir -p "$HOME/.config/nvim/"
+mkdir -p "$HOME/.config/ghostty/"
 
-ln -s -f "$CWD/.local/bin/tmux-sessionizer" "$LOCALBIN/tmux-sessionizer"
 ln -s -f "$CWD/.tmux.conf" "$TMUX"
-ln -s -f "$CWD/init.vim" "$NVIM"
+ln -s -f "$CWD/.config/nvim/init.vim" "$NVIM"
+ln -s -f "$CWD/.config/ghostty/init.vim" "$GHOSTTY"
 ln -s -f "$CWD/.zshrc" "$ZSH"
 
 # ZSH plugins 
-echo -e "Installing [${color_light_green}ZSH Plugins${color_nc}]";
-cp -uf zsh/prompt.sh "$zshdir" 2> /dev/null && echo -e "ZSH prompt script [${color_light_green}Installed${color_nc}]";
+echo -e "Installing [ZSH Plugins]";
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" # Oh my zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions  # autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting # syntax-highlighting
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" # ZPrezto
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm # Tmux Plugin Manager 
-git clone https://github.com/romkatv/powerlevel10k.git ${ZDOTDIR:-$HOME}/.oh-my-zsh/themes/powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git "${ZDOTDIR:-$HOME}/.oh-my-zsh/themes/powerlevel10k"
 
 # Finishing up
-echo -e "${color_light_green}All Set-up!${color_nc}";
+echo -e "All Set-up!";
