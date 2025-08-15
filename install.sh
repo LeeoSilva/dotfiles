0d43e4438bb4ddbc58642907929c07bf4cba4835
LeeoSilva <leeosilva0909@gmail.com>
2025-08-15 16:05:17 -0300

    Removing error messages.

--- install.sh
+++ install.sh
@@ -10,19 +10,14 @@ mkdir -p "$HOME/.config/"
 mkdir -p "$HOME/.config/nvim/"
 mkdir -p "$HOME/.config/ghostty/"
 
-ln -s -f "$CWD/.tmux.conf" "$TMUX"
-ln -s -f "$CWD/.config/nvim/init.vim" "$NVIM"
-ln -s -f "$CWD/.config/ghostty/config" "$GHOSTTY"
-ln -s -f "$CWD/.zshrc" "$ZSH"
+ln -sf "$CWD/.tmux.conf" "$TMUX"
+ln -sf "$CWD/.config/nvim/init.vim" "$NVIM"
+ln -sf "$CWD/.config/ghostty/config" "$GHOSTTY"
+ln -sf "$CWD/.zshrc" "$ZSH"
 
-# ZSH plugins 
-echo -e "Installing [ZSH Plugins]";
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" # Oh my zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions  # autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting # syntax-highlighting
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" # ZPrezto
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm # Tmux Plugin Manager 
-git clone https://github.com/romkatv/powerlevel10k.git "${ZDOTDIR:-$HOME}/.oh-my-zsh/themes/powerlevel10k"
+[ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+[ ! -d "$HOME/.zsh/zsh-autosuggestions" ] && git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.zsh/zsh-autosuggestions" 
+[ ! -d "$HOME/.zsh/zsh-syntax-highlighting" ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh/zsh-syntax-highlighting" 
+[ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ] && git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" 
+[ ! -d "${ZDOTDIR:-$HOME}/.oh-my-zsh/themes/powerlevel10k" ] && git clone https://github.com/romkatv/powerlevel10k.git "${ZDOTDIR:-$HOME}/.oh-my-zsh/themes/powerlevel10k" 
 
-# Finishing up
-echo -e "All Set-up!";
